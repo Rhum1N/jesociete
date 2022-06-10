14c255cfc79a3819c667bec227a308ee6c6a09b0
Rhum1N <60776729+Rhum1N@users.noreply.github.com>
2022-06-10 16:29:24 +0200

    Update 6kipren.py
    bonjour à tous

--- 6kipren.py
+++ 6kipren.py
@@ -19,13 +19,19 @@ player_number = 5
 
 
 ##Init
-board = np.zeros((4,5))
+#
+board = np.zeros((4,5)) 
 board[:,0] = r.sample(cards,4)
 for i in range(4):
     cards.remove(board[i,0])
 
 hands = r.sample(cards,player_number*10)
 hands = np.reshape(hands,(player_number,10))
+
+def next_state(board,cards) : #computes the next state of the board 
+    #comment pas trop salement retrouver qui a poser quelle carte quand il doit perdre des points ?
+    return "holé"
+
 
 #players init
 play = []
@@ -35,4 +41,8 @@ for i in range(player_number) :
 ##Game on
 cards_played = np.zeros(player_number)
 for i in range(10):
-    cards_played[i] = play.
+    for j in range(player_number):
+        cards_played[i] = p[i].move(board)
+        
+    
+    
